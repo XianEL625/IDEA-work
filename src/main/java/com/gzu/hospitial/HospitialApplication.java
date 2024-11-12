4ecddb763f5b7100a76969864c06ea6870bd761f
仙.EL <2506263100@qq.com>
2024-11-12 11:15:59 +0800

    javaweb大作业第一次启动

---
+++ src/main/java/com/gzu/hospitial/HospitialApplication.java
@@ -0,0 +1,13 @@
+package com.gzu.hospitial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gzu/hospitial/HospitialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzu.hospitial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
